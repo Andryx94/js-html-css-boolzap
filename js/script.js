65b917e0199bb0344bfdd86bfd5bdff56a9981a5
Andrea <andryx1994@gmail.com>
2020-06-16 18:53:22 +0200

    Versione 0.3
    Aggiunto codice JS invio messaggio ed effettistica

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 $(document).ready(
   function() {
+    var inputMessaggio = $(".right-message .text-input input");
+    var microfono = $("#microfono");
+    var telegram = $("#telegram");
+
+    inputMessaggio.focus(function() {
+      microfono.toggleClass("hidden");
+      telegram.toggleClass("hidden");
+    });
+
+    // inputMessaggio.focusout(function() {
+    //   microfono.toggleClass("hidden");
+    //   telegram.toggleClass("hidden");
+    // });
+
+    telegram.click(function() {
+      invioMessaggio(inputMessaggio);
+      inputMessaggio.val("");
+    });
+
+    inputMessaggio.keyup(function() {
+      if ( event.which == 13 ) {
+        invioMessaggio(inputMessaggio);
+        inputMessaggio.val("");
+      }
+    });
   }
 );
+
+function invioMessaggio(inputMessaggio) {
+  var input = $(".right-chat .chat .template p").text(inputMessaggio.val());
+  var clone = $(".right-chat .chat .template").clone();
+  clone.removeClass("template hidden");
+  $(".right-chat .chat").append(clone);
+};
